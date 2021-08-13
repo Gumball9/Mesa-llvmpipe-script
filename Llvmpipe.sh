2806d735896baf05142fb1a93a2ef38cec2f9e82
Gumball9 <87555282+Gumball9@users.noreply.github.com>
2021-08-13 19:37:39 +0530

    Update Llvmpipe.sh

--- Llvmpipe.sh
+++ Llvmpipe.sh
@@ -8,9 +8,9 @@ sudo apt update && apt upgrade -y
 #Installing Dependencies
 sudo apt build-dep mesa -y
 sudo apt build-dep llvm -y
-sudo apt install llvm-10 -y
+sudo apt install llvm-12 -y
 sudo apt install libvulkan-dev -y
-sudo apt install python python3 python3-mako -y python3-pip
+sudo apt install python python3 python3-mako python3-pip -y
 sudo apt install build-essential -y
 sudo pip3 install meson bison flex -y
 
@@ -24,16 +24,15 @@ tar -xf mesa-21.2.0.tar.xz
 cd mesa-21.2.0
 mkdir build && cd build
 meson build
-ninija
+ninja
 sudo ninja install
 clear
 
 echo "Done"
 
-echo "now check your mesa version if it's 21.2.0 then operation done successfully but if not try again and report the problem"
+
 
 #checking mesa version
 
 glxinfo | grep Mesa
 
-#tweaks
